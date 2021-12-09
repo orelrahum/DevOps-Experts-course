36cf93f9a6d67f1811cc0597d2c681839b5916ea
orelrahum <44848947+orelrahum@users.noreply.github.com>
2021-12-09 12:17:49 +0200

    solving task1

--- Lesson 1/Solution.py
+++ Lesson 1/Solution.py
@@ -1,18 +1,22 @@
 # My Solution for assignment  1
 # Wrote By Orel Rahum
 
-# Create a variable name first with value 7.
+##################################################################################################
+# Part A
+print("##########################################################################################")
+print("                                      part A ")
 first = 7
-
-# Create a variable name second with value 44.3.
 second = 44.3
-
 # print results
 print(f"first value is :{first}\nsecond value is {second}")
 print(f"result of adding first to second :{first + second}")
 print(f"result of multiplying first by second :{first * second}")
 print(f"result of dividing second by first :{second / first}")
 
+##################################################################################################
+# Part B
+print("\n##########################################################################################")
+print("                                      part B ")
 a = 8
 a = 17
 a = 9
@@ -20,6 +24,31 @@ b = 6
 c = a + b
 b = c + a
 b = 8
-
 # print results
 print(f"a value is {a}\nb value is {b}\nc value is {c}")
+
+##################################################################################################
+# Part C
+print("\n##########################################################################################")
+print("                                      part C ")
+# No, In Python it is the same
+
+# The problem in the code is that we tried to add string to int
+my_number = 5 + 5
+print("result is: " + str(my_number))
+
+##################################################################################################
+# Part D
+print("\n##########################################################################################")
+print("                                      part D ")
+x = 5
+y = 2.36
+print(f"the result of x + int(y) is: {x + int(y)}")
+
+##################################################################################################
+# CHALLENGE
+print("\n##########################################################################################")
+print("                                     CHALLENGE")
+a = 8
+b = "123"
+print(f"the result is : {a + int(b)}")
