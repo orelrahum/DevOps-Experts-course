e4ef8d5d71d5d8f5b82232cb557b41b6a4c4fca0
orelrahum <44848947+orelrahum@users.noreply.github.com>
2021-12-09 12:00:52 +0200

    first Assignment solving

---
+++ Lesson 1/Solution.py
@@ -0,0 +1,25 @@
+# My Solution for assignment  1
+# Wrote By Orel Rahum
+
+# Create a variable name first with value 7.
+first = 7
+
+# Create a variable name second with value 44.3.
+second = 44.3
+
+# print results
+print(f"first value is :{first}\nsecond value is {second}")
+print(f"result of adding first to second :{first + second}")
+print(f"result of multiplying first by second :{first * second}")
+print(f"result of dividing second by first :{second / first}")
+
+a = 8
+a = 17
+a = 9
+b = 6
+c = a + b
+b = c + a
+b = 8
+
+# print results
+print(f"a value is {a}\nb value is {b}\nc value is {c}")
